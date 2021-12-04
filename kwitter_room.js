cbb2169e355088ceee545d6a5a466e900d159973
charliegautam <90816807+charliegautam@users.noreply.github.com>
2021-12-03 16:20:03 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,6 +1,6 @@
 
 //ADD YOUR FIREBASE LINKS HERE
-const firebaseConfig = {
+var firebaseConfig = {
       apiKey: "AIzaSyBIugSzkJvPVW9kXs7pB-UAc5kttXDlY9k",
       authDomain: "kwitter-910d0.firebaseapp.com",
       databaseURL: "https://kwitter-910d0-default-rtdb.firebaseio.com",
@@ -11,12 +11,17 @@ const firebaseConfig = {
     };
     
     // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
     
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      
+function addUser(){
+      userName = document.getElementById("enter_room").value;
+      firebase.database().ref("/").child(userName).update({
+            purpose : "Add user name"});
+        }
       //End code
       });});}
 getData();
